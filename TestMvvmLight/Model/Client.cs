6a86ec8b750aef000981d052c7333c983368a70b
Alexandre Engelhardt <alexandre@zipion-live.be>
2013-06-14 16:58:48 +0200

    No link between View and ViewModel yet
    This project is based on the following tutorial :
    
    http://www.siteduzero.com/informatique/tutoriels/apprenez-a-developper-pour-windows-phone-en-c/les-frameworks-a-la-rescousse-mvvm-light

---
+++ TestMvvmLight/Model/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TestMvvmLight.Model
+{
+    public class Client
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool IsGoodClient { get; set; }
+
+        public Client(string FirstName, string LastName, int Age, bool IsGoodCustomer)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Age = Age;
+            this.IsGoodClient = IsGoodClient;
+        }
+    }
+
+    public interface IClientService
+    {
+        Client Load();
+    }
+
+    public class ClientService : IClientService
+    {
+        public Client Load()
+        {
+            return new Client("Alexandre", "Engelhardt", 29, true);
+        }
+    }
+}
+
+namespace TestMvvmLight.Model.Design
+{
+    public class DesignClientService : IClientService
+    {
+        public Client Load()
+        {
+            return new Client("Alexandre", "Engelhardt", 29, true);
+        }
+    }
+}
---
+++ TestMvvmLight/ViewModelLocator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Practices.ServiceLocation;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using TestMvvmLight.Model;
+using TestMvvmLight.Model.Design;
+using TestMvvmLight.ViewModel;
+
+namespace TestMvvmLight
+{
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            if (ViewModelBase.IsInDesignModeStatic)
+                SimpleIoc.Default.Register<IClientService, DesignClientService>();
+            else
+                SimpleIoc.Default.Register<IClientService, ClientService>();
+
+            SimpleIoc.Default.Register<ShowClientViewModel>();
+        }
+
+        public ShowClientViewModel ShowClientVM
+        {
+            get { return ServiceLocator.Current.GetInstance<ShowClientViewModel>(); }
+        }
+    }
+}
